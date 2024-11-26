e9d8981b12495ddac08bf1f548517b6ee3790982
aldaxho <65668485+aldaxho@users.noreply.github.com>
2024-11-25 23:25:51 -0400

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
+            CategoriaSeeder::class,
             RolesTableSeeder::class,
             UsuariosTableSeeder::class,
             SubscripcionesTableSeeder::class,
